88e6bb020f4ca1aafb602fc89faeaae2fac6fc85
creativeorange3 <creativeorange3@gmail.com>
2022-04-22 16:43:34 +0200

    ca

--- src/components/Stages/End/Intro.js
+++ src/components/Stages/End/Intro.js
@@ -12,6 +12,7 @@ const Intro = React.memo(props => (
   >
     <h2 style={{ color: "black" }}>Welcome to 8bitcro, NFT based exploration Game</h2>
     <h3 style={{ color: "black" }}><a href="https://mm.finance/swap?inputCurrency=0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23&outputCurrency=0x7a071a504ac437f4a48c72f5bfb327b54e90a0bc" target={"_blank"}>BUY HERE</a></h3>
+    <h3 style={{ color: "black" }}><a href="https://cronoscan.com/address/0x7a071a504ac437f4a48c72f5bfb327b54e90a0bc" target={"_blank"}>CONTRACT</a></h3>
   </SceneObject>
 ));
 export default Intro;
